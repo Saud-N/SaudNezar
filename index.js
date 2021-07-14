c6a27ba4d11ede89c59519cfe5b375dc1264c992
Saud Nezar <saudnezar060@gmail.com>
2021-07-15 01:47:02 +0300

    version 1.1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express(); // web server
 const port = process.env.PORT || 8000;
 
-app.use(express.static('public')) 
+app.use(express.static('build')) 
 
 app.listen(port)
